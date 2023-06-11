4f70138c56f3721bc8b943b9913a596cdfa08514
samdani-srabon <55274357+samdani-srabon@users.noreply.github.com>
2023-06-11 17:33:43 +0000

    segemnt

---
+++ main.js
@@ -0,0 +1,8 @@
+function updateTime() {
+    const now = new Date();
+    const londonTime = now.toLocaleString("en-US", {timeZone: "Europe/London", hour: "numeric", minute: "numeric", second: "numeric", hour12: true});
+    const newyorkTime = now.toLocaleString("en-US", {timeZone: "America/New_York", hour: "numeric", minute: "numeric", second: "numeric", hour12: true});
+    document.getElementById("london-time").innerHTML = londonTime;
+    document.getElementById("newyork-time").innerHTML = newyorkTime;
+  }
+  setInterval(updateTime, 10);
